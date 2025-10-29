fb134ca487e214d264e64c2567ac5b3300f23537
Vishnu Ramadas <vramadas@outlook.com>
2025-10-29 14:34:43 -0500

    Add util scripts

---
+++ utils/Plots.py
@@ -0,0 +1,355 @@
+#!/opt/homebrew/bin/python3.12
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import os
+from fractions import Fraction
+import pandas as pd
+import numpy as np
+
+plot_base_dir = "plots/"
+plot_save_dir = ""
+
+def set_save_path(dir_name):
+    global plot_base_dir
+    global plot_save_dir
+    plot_save_dir = os.path.join(plot_base_dir, dir_name)
+    if not os.path.exists(plot_save_dir): 
+        os.makedirs(plot_save_dir)
+    
+
+def plot_mrc(workload_set, benchmarks, stats_list, graph):
+    # Implementation for MRC scatter plot
+    for stats in stats_list:
+        df_list = [
+            pd.DataFrame({
+                "Cache Size": stats.mrc[expt].cache_sizes,
+                "Miss Rate": stats.mrc[expt].miss_rate,
+                "Benchmark": expt,
+            })
+            for expt in benchmarks
+            if expt in stats.expts.keys()
+        ]        
+
+        df_long = pd.concat(df_list, ignore_index=True)
+
+        sns.lineplot(
+            data=df_long,
+            x="Cache Size",      # The shared x-axis variable
+            y="Miss Rate",     # The y-axis variable
+            hue="Benchmark", # The categorical variable that defines each line
+            palette="Paired",
+            marker='o',
+        )
+        plt.xscale('log', base=2)
+
+        formatter = ticker.FuncFormatter(lambda v, pos: Fraction(v).limit_denominator())
+        #Apply the formatter to the y-axis major ticks
+        plt.gca().xaxis.set_major_formatter(formatter)
+
+        plt.ylim(0, 1.0)
+        plt.xlabel("Cache Size (KB)")
+        plt.ylabel("Miss Rate")
+        plt.title(f"MRC for {workload_set} (Cache Block Size {stats.block_size}B)")
+        save_path = os.path.join(plot_save_dir, f"{workload_set}_mrc_blkSize_{stats.block_size}.png")
+        plt.savefig(save_path, bbox_inches='tight')
+        plt.close()
+    return
+
+def plot_footprint(workload_set, benchmarks, stats_list, graph):
+        df = pd.concat([
+            pd.DataFrame({
+                "Footprint": [(stats.expts[expt].cold_misses * stats.block_size) / (1024.0 * 1024.0)],
+                "Benchmark": [expt],
+                "Cache Block Size": [stats.block_size],
+            })
+            for expt in benchmarks
+            for stats in stats_list
+            if expt in stats.expts.keys()
+        ])
+
+        hue_order = sorted(df['Cache Block Size'].unique(), 
+                           reverse=True)
+        
+        df = df.sort_values(
+            by=["Footprint"],
+            ascending=False,
+        )
+
+        ax = sns.barplot(
+            data=df,
+            x="Benchmark",      # The shared x-axis variable
+            y="Footprint",     # The y-axis variable
+            hue="Cache Block Size", # The categorical variable that defines each line
+            palette="pastel",
+            hue_order=hue_order,
+            #marker='o',
+        )
+        
+        plt.ylim(1, 1*1024)
+        plt.yscale('log', base=2)
+
+        formatter = ticker.FuncFormatter(lambda v, pos: f"{Fraction(v).limit_denominator()} MB")
+        #Apply the formatter to the y-axis major ticks
+        plt.gca().yaxis.set_major_formatter(formatter)
+
+        vertical_offset = 0.1
+        for patch in ax.patches:
+            # Get the height (value) and the center x-position of the bar
+            height = patch.get_height()
+            x = patch.get_x() + patch.get_width() / 2
+            # Format the label (e.g., two decimal places)
+            label = f'{height:.2f} MB'
+
+            if x == 0 and height == 0:
+                continue
+
+            # Simple placement slightly above the bar center:
+            ax.text(
+                x,                      # x-coordinate of the bar center
+                height + vertical_offset, # y-coordinate (just above the bar)
+                label,                  # The text label
+                ha='center',            # Horizontal alignment (center the text on the bar)
+                va='bottom',            # Vertical alignment (align the bottom of the text)
+                fontsize=8,             # Adjust font size as needed
+                rotation=45             # Rotate the text for better overlap prevention
+            )
+
+        plt.title(f"Memory Footprint for {workload_set}")
+        plt.xlabel("Benchmark")
+        plt.xticks(rotation=45)
+        plt.ylabel("Footprint (KB)")
+        save_path = os.path.join(plot_save_dir, f"{workload_set}_footprint.png")
+        plt.savefig(save_path)#, bbox_inches='tight')
+        plt.close()
+        return
+
+def plot_utilization(workload_set, benchmarks, stats_list, graph, hue_order):
+    df = pd.concat([
+        pd.DataFrame({
+            "Utilization": [stats.expts[expt].cache_line_utilization * 100],
+            "Benchmark": [expt],
+            "Cache Block Size": [stats.block_size],
+            "Legend": [stats.desc],
+        })
+        for expt in benchmarks
+        for stats in stats_list
+        if expt in stats.expts.keys()
+    ])
+    #hue_order = sorted(df['Cache Block Size'].unique(), 
+    #                   reverse=True)
+
+    df = df.sort_values(
+        by=["Cache Block Size", "Utilization"],
+        ascending=False,
+    )
+
+    ax = sns.barplot(
+        data=df,
+        x="Benchmark",      # The shared x-axis variable
+        y="Utilization",     # The y-axis variable
+        hue="Legend", # The categorical variable that defines each line
+        palette="pastel",
+        hue_order=hue_order,
+        #marker='o',
+    )
+    
+    plt.ylim(0, 100.0)
+
+    vertical_offset = 0.1
+    for patch in ax.patches:
+        # Get the height (value) and the center x-position of the bar
+        height = patch.get_height()
+        x = patch.get_x() + patch.get_width() / 2
+        # Format the label (e.g., two decimal places)
+        label = f'{height:.2f}%'
+        
+        if x == 0 and height == 0:
+            continue
+
+        # Simple placement slightly above the bar center:
+        ax.text(
+            x,                      # x-coordinate of the bar center
+            height + vertical_offset, # y-coordinate (just above the bar)
+            label,                  # The text label
+            ha='center',            # Horizontal alignment (center the text on the bar)
+            va='bottom',            # Vertical alignment (align the bottom of the text)
+            fontsize=8,             # Adjust font size as needed
+            rotation=45             # Rotate the text for better overlap prevention
+        )
+
+    plt.title(f"Cache Line Utilization for {workload_set}")
+    plt.xlabel("Benchmark")
+    plt.xticks(rotation=45)
+    plt.ylabel("Cache Line Utilization (%)")
+    plt.tight_layout()
+    save_path = os.path.join(plot_save_dir, f"{workload_set}_utilization.png")
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.close()
+    return
+
+def plot_mpki(workload_set, benchmarks, stats_list, graph, hue_order):
+    df = pd.concat([
+        pd.DataFrame({
+            "MPKI": [(stats.expts[expt].misses - stats.expts[expt].mshr_merge)*1000.0/stats.expts[expt].num_instructions],
+            "Benchmark": [expt],
+            "Cache Block Size": [stats.block_size],
+            "Legend": [stats.desc],
+        })
+        for expt in benchmarks
+        for stats in stats_list
+        if expt in stats.expts.keys()
+    ])
+    #hue_order = sorted(df['Cache Block Size'].unique(), 
+    #                   reverse=True)
+
+    df = df.sort_values(
+        by=["Cache Block Size", "MPKI"],
+        ascending=False
+    )         
+    ax = sns.barplot(
+        data=df,
+        x="Benchmark",      # The shared x-axis variable
+        y="MPKI",     # The y-axis variable
+        hue="Legend", # The categorical variable that defines each line
+        palette="pastel",
+        hue_order=hue_order,
+        #marker='o',
+    )
+    
+    plt.ylim(0, 100.0)
+    vertical_offset = 0.1
+    for patch in ax.patches:
+        # Get the height (value) and the center x-position of the bar
+        height = patch.get_height()
+        x = patch.get_x() + patch.get_width() / 2
+        # Format the label (e.g., two decimal places)
+        label = f'{height:.2f}'
+        
+        if x == 0 and height == 0:
+            continue
+
+        # Simple placement slightly above the bar center:
+        ax.text(
+            x,                      # x-coordinate of the bar center
+            height + vertical_offset, # y-coordinate (just above the bar)
+            label,                  # The text label
+            ha='center',            # Horizontal alignment (center the text on the bar)
+            va='bottom',            # Vertical alignment (align the bottom of the text)
+            fontsize=8,             # Adjust font size as needed
+            rotation=45             # Rotate the text for better overlap prevention
+        )
+
+    plt.title(f"MPKI for {workload_set}")
+    plt.xlabel("Benchmark")
+    plt.xticks(rotation=45)
+    plt.ylabel("MPKI")
+    plt.tight_layout()
+    save_path = os.path.join(plot_save_dir, f"{workload_set}_mpki.png")
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.close()
+    return
+
+def plot_evictions_breakdown(workload_set, benchmarks, stats_list, graph):
+    for stats in stats_list:
+        df = pd.concat([
+            pd.DataFrame({
+                "Evictions Breakdown": [{key: 100.0*value/stats.expts[expt].total_evictions for key, value in stats.expts[expt].evictions_breakdown.items()}],
+                "Benchmark": [expt],
+            })
+            for expt in benchmarks
+            if expt in stats.expts.keys()
+        ])
+
+        evictions_expanded = df['Evictions Breakdown'].apply(pd.Series)
+        df_expanded = pd.concat([df.drop(columns=['Evictions Breakdown']), evictions_expanded], axis=1).set_index('Benchmark')
+
+        #palette = sns.color_palette('Paired', n_colors=16)
+        # Plot stacked bar plot on each subplot
+        ax = df_expanded.plot(kind='bar', 
+                                  stacked=True,
+                                  colormap='Paired',
+                                  #order=sorted(df["benchmark"].unique()))
+                            )
+        ax.set_title(f"Evictions Breakdown (Block Size: {stats.block_size} Bytes)")
+        ax.set_xlabel("Benchmarks")
+        ax.set_ylabel("Percentage of Cache Line Used (%)")
+        #plt.legend(title="Number of Blocks Used")
+        plt.xticks(np.arange(len(benchmarks)),benchmarks)
+        plt.tick_params(axis='x', rotation=90)  # rotate x-axis labels
+        ax.set_ylim(0, 100)
+        plt.legend(
+            title="Number of Blocks Used in Cache Line",
+            loc='lower center',
+            bbox_to_anchor=(1.0, 0.0),
+        )
+        plt.tight_layout()
+        save_path = os.path.join(plot_save_dir, f'{workload_set}_evictions_breakdown_blkSize_{stats.block_size}.png')
+        plt.savefig(save_path, bbox_inches='tight')
+        plt.close()
+    return
+
+def plot_ipc(workload_set, benchmarks, stats_list, graph, hue_order):
+    df = pd.concat([
+        pd.DataFrame({
+            "IPC": [stats.expts[expt].ipc],
+            "Benchmark": [expt],
+            "Cache Block Size": [stats.block_size],
+            "Legend": [stats.desc],
+        })
+        for expt in benchmarks
+        for stats in stats_list
+        if expt in stats.expts.keys()
+    ])
+    #hue_order = sorted(df['Cache Block Size'].unique(), 
+    #                   reverse=True)
+
+    df = df.sort_values(
+        by=["Cache Block Size", "IPC"],
+        ascending=False,
+    )
+
+    ax = sns.barplot(
+        data=df,
+        x="Benchmark",      # The shared x-axis variable
+        y="IPC",     # The y-axis variable
+        hue="Legend", # The categorical variable that defines each line
+        palette="pastel",
+        hue_order=hue_order,
+        #marker='o',
+    )
+    
+    plt.ylim(0, 2.0)
+
+    vertical_offset = 0.1
+    for patch in ax.patches:
+        # Get the height (value) and the center x-position of the bar
+        height = patch.get_height()
+        x = patch.get_x() + patch.get_width() / 2
+        # Format the label (e.g., two decimal places)
+        label = f'{height:.2f}'
+        
+        if x == 0 and height == 0:
+            continue
+
+        # Simple placement slightly above the bar center:
+        ax.text(
+            x,                      # x-coordinate of the bar center
+            height + vertical_offset, # y-coordinate (just above the bar)
+            label,                  # The text label
+            ha='center',            # Horizontal alignment (center the text on the bar)
+            va='bottom',            # Vertical alignment (align the bottom of the text)
+            fontsize=8,             # Adjust font size as needed
+            rotation=45             # Rotate the text for better overlap prevention
+        )
+
+    plt.title(f"IPC for {workload_set}")
+    plt.xlabel("Benchmark")
+    plt.xticks(rotation=45)
+    plt.ylabel("IPC")
+    plt.tight_layout()
+    save_path = os.path.join(plot_save_dir, f"{workload_set}_ipc.png")
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.close()
+    return
+
---
+++ utils/Stats.py
@@ -0,0 +1,211 @@
+import os
+import re
+
+class CacheStats:
+    class ExptStats:
+        def __init__(self):
+            self.hit_rate = 0
+            self.total_hit_rate = 0
+            self.unrealised_hit_rate = 0
+            self.total_unrealised_hit_rate = 0
+            self.hits = 0
+            self.misses = 0
+            self.accesses = 0
+            self.total_misses = 0
+            self.total_hits = 0
+            self.total_accesses = 0
+            self.partial_hits = 0
+            self.partial_misses = 0
+            self.unrealised_hits = 0
+            self.total_unrealised_hits = 0
+            self.cold_misses = 0
+            self.capacity_misses = 0
+            self.conflict_misses = 0
+            self.cache_line_utilization = 0
+            self.total_cache_line_utilization = 0
+            self.evictions = 0
+            self.total_evictions = 0
+            self.mshr_merge = 0
+            self.evictions_breakdown = {}
+            self.simpoints_inst_dropped = ""
+            self.num_instructions = 0
+            self.ipc = 0
+            self.suite = ""
+            self.graph = ""
+            self.desc = ""
+            return
+
+    class MRCStats:
+        def __init__(self):
+            self.cache_sizes = []
+            self.cumulative_hits = []
+            self.misses = []
+            self.miss_rate = []
+            self.suite = ""
+            self.graph = ""
+            return
+        
+    def __init__(self, cache_name, cache_size = "2MB", block_size = 64):#, exptList):
+        self.expts = {}
+        self.mrc = {}
+        self.cache_name = cache_name
+        self.cache_size = cache_size
+        self.block_size = block_size
+        self.exptList = []
+    
+    def setEvictionsBreakdown(self, expt, line: str):
+        pattern = r'(\d+):\s+(\d+)'
+        matches = re.findall(pattern, line)
+        for key, value in matches:
+            expt.evictions_breakdown[key] = int(value)
+        return
+
+
+    def setExptStats(self, expt: str, stat_file: str, suite: str, graph: str):
+        start_stats = False
+        if expt not in self.expts.keys():
+            self.expts[expt] = self.ExptStats()
+            self.expts[expt].suite = suite
+            self.expts[expt].graph = graph
+        
+        f = open(stat_file, 'r')
+        for line in f:
+            if not start_stats and "Region of Interest" in line:
+                start_stats = True
+                #next(f)
+                continue
+            
+            if start_stats:
+                line_list = line.split()
+                if len(line_list) < 2:
+                   # Empty line. Skip it
+                   continue
+
+                if "IPC:" in line_list:
+                   self.expts[expt].ipc = float(line_list[4])
+
+                if self.expts[expt].num_instructions == 0:
+                    match = re.search(r"instructions: (\d+)", line)
+                    if match:
+                        self.expts[expt].num_instructions = int(match.group(1))
+                    continue
+                
+                match = re.match(r"cpu0->(?:cpu0_)?(.+)", line_list[0])
+                if not match:
+                    continue
+                cache = match.group(1)
+                if cache != self.cache_name:
+                   continue
+
+                if "TOTAL" != line_list[1]:
+                   # Not aggregate stats. Skip it
+                   return
+
+                if "TOTAL_ACCESS:" == line_list[2]:
+                    self.expts[expt].total_accesses = int(line_list[3])
+                    self.expts[expt].total_hits = int(line_list[5])
+                    self.expts[expt].total_misses = int(line_list[7])
+                    self.expts[expt].unrealised_hits = int(line_list[9])
+                    self.expts[expt].total_hit_rate = float(self.expts[expt].total_hits/self.expts[expt].total_accesses)
+                    self.expts[expt].unrealised_hit_rate = float(line_list[13])
+                    self.expts[expt].total_unrealised_hits = int(line_list[11])
+                    self.expts[expt].total_unrealised_hit_rate = float(line_list[15])
+                elif "EVICTIONS:" == line_list[2]:
+                    self.expts[expt].evictions = int(line_list[3])
+                    self.expts[expt].total_evictions = int(line_list[5])
+                    if self.expts[expt].evictions == 0:
+                        self.expts[expt].evictions = 1
+                    if self.expts[expt].total_evictions == 0:
+                        self.expts[expt].total_evictions = 1
+                    self.expts[expt].cache_line_utilization = 1 - (float(int(line_list[7])/self.expts[expt].evictions)/8)
+                    self.expts[expt].total_cache_line_utilization = 1 - (float(int(line_list[9])/self.expts[expt].total_evictions)/8)
+                elif "ACCESS:" == line_list[2]:
+                    self.expts[expt].accesses = int(line_list[3])
+                    self.expts[expt].hits = int(line_list[5])
+                    self.expts[expt].misses = int(line_list[7])
+                    self.expts[expt].cold_misses = int(line_list[9])
+                    self.expts[expt].capacity_misses = int(line_list[11])
+                    self.expts[expt].conflict_misses = int(line_list[13])
+                    self.expts[expt].mshr_merge = int(line_list[15])
+                elif "EVICTIONS_BREAKDOWN" == line_list[2]:
+                    self.setEvictionsBreakdown(self.expts[expt], line)
+                elif "PARTIAL_HITS:" == line_list[2]:
+                    self.expts[expt].partial_hits = int(line_list[3])
+                    self.expts[expt].partial_misses = int(line_list[5])
+        return
+
+    def setMRCStats(self, expt: str, stat_file: str, suite: str, graph: str):
+        start_stats = False
+        f = open(stat_file, 'r')
+        if expt not in self.mrc.keys():
+            self.mrc[expt] = self.MRCStats()
+            self.expts[expt].suite = suite
+            self.expts[expt].graph = graph
+        for line in f:
+            if "Miss Ratio Curve (MRC)" in line and self.cache_name in line:
+                start_stats = True
+                next(f)
+                next(f)
+                continue
+            if start_stats:
+                line_list = line.split('|')
+                if len(line_list) != 4:
+                    # End of MRC stats
+                    return
+
+                self.mrc[expt].cache_sizes.append(float(line_list[0].strip())*self.block_size/1024.0)
+                self.mrc[expt].cumulative_hits.append(int(line_list[1].strip()))
+                self.mrc[expt].misses.append(int(line_list[2].strip()))
+                self.mrc[expt].miss_rate.append(float(line_list[3].strip()))
+        return
+    
+    def populateStats(self, results_dir, graph, block_size, num_ways, multiple_traces=False):        
+        stats_dir = os.path.join("results", results_dir, "expt", "outputs", f"blkSize_{block_size}_way_{num_ways}")
+        baseDir = os.getcwd()
+        os.chdir(os.path.join(baseDir, stats_dir))
+        self.block_size = block_size
+        for expt_file in os.listdir(os.getcwd()):
+            f = open(os.path.abspath(expt_file), 'r')
+            pattern = r'^[^_]+'
+            match = re.match(pattern, expt_file)
+            if match is None:
+                continue
+
+            inst_dropped_pattern = ""
+            if "ligra" in expt_file:
+                pattern = pattern = r"[^.]+\.([^.]+)"
+                match = re.match(pattern, expt_file)
+                if match.group(1) != graph:
+                    continue
+
+                pattern = r"^ligra_ligra_(.*?)\."
+                inst_dropped_pattern = r'drop_(\d+M)\.'
+                suite = "ligra"
+                graph = re.match(r"^[^.]+\.([^\.]+)\.", expt_file).group(1)
+            elif "spec2006" in expt_file:
+                pattern = r"^spec2006_\d+\.(\w+)-.*\.champsimtrace\.xz\.stdout$"
+                suite = "spec2006"
+                inst_dropped_pattern = r'(\d+B)\.champsimtrace'
+            elif "spec2017" in expt_file:
+                pattern = r"^spec2017_\d+\.(\w+)-.*\.champsimtrace\.xz\.stdout$"
+                suite = "spec2017"
+                inst_dropped_pattern = r'(\d+B)\.champsimtrace'
+            elif "parsec" in expt_file:
+                #pattern = r"\.([a-zA-Z]+)\.simlarge"
+                #suite = "parsec"
+                continue
+            else:
+                pattern = r'^([^.]+)'
+                suite = "ubench"
+            expt = (re.match(pattern, expt_file).group(1))
+            simpoints_inst_dropped = ""
+            if inst_dropped_pattern != "" and multiple_traces:
+                simpoints_inst_dropped = re.search(inst_dropped_pattern, expt_file).group(1) 
+                expt = '_'.join([expt, simpoints_inst_dropped])
+            self.exptList.append(expt)
+            self.setExptStats(expt, os.path.abspath(expt_file), suite, graph)
+            self.setMRCStats(expt, os.path.abspath(expt_file), suite, graph)
+            self.expts[expt].simpoints_inst_dropped = simpoints_inst_dropped
+
+            f.close()
+        os.chdir(baseDir)
